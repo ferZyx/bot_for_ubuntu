de8a6406719c1b4b2e66620c9f9d8a67fcd35ae6
ferZyx <sorokin.wlad@yandex.ru>
2023-05-25 18:55:16 +0600

    ourth commit

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ TOKEN = "5678089959:AAFCg7wHG1JGPDeBo_ImzoYSULVn-RYW0rg"
 
 bot = telebot.TeleBot(token=TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(commands=['hi'])
+def start_message(message):
+    bot.reply_to(message, "Топассын ба?")
 
 
 bot.polling(non_stop=True)
